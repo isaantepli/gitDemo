bc4f356bfbd0106f079e009ee0f6a6e348a05c96
isaantepli <isaantepli46@gmail.com>
2020-08-01 09:56:28 -0600

    update code

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,6 +1,12 @@
 public class HappyB20 {
     public static void main(String[] args) {
-        System.out.println("Happy August");
+        System.out.println("Happy August!!!");
+
+        int a = 5;
+        int b = 10;
+        int result = a+b;
+        System.out.println(result);
+        // add
 
 
     }
