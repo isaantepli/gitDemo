e0669a4260cb7579eaa00b7ca89e39621c7eea10
isaantepli <66892614+isaantepli@users.noreply.github.com>
2020-08-01 14:02:02 -0600

    Update BookARoom.java

--- src/BookARoom.java
+++ src/BookARoom.java
@@ -1,6 +1,9 @@
 public class BookARoom {
     public static void main(String[] args) {
         System.out.println("Book a room");
+        
+        System.out.println("Hello Java!!!");
+        
     }
 }
 
